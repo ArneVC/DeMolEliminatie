cfaeebd607dea4374a291aa37bb7ff0316a3e887
Arne <arnev.c.23@gmail.com>
2025-05-06 13:57:41 +0200

    setup routing for main

--- DeMol/src/app/app.routes.ts
+++ DeMol/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ElimpageComponent } from './elimpage/elimpage.component';
 import { ResultspageComponent } from './resultspage/resultspage.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
+    { path: 'main', component: MainComponent },
     { path: 'elim', component: ElimpageComponent },
     { path: 'result', component: ResultspageComponent },
+    { path: '**', redirectTo: 'main' },
 ];
