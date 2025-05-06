2e814dbbab1518911d62514a9bf055f1aaaf9767
Arne <arnev.c.23@gmail.com>
2025-05-06 14:19:47 +0200

    setup main page form

--- DeMol/src/app/main/main.component.ts
+++ DeMol/src/app/main/main.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ElimserviceService } from '../services/elimservice.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  people: string[] = [];
+  constructor(private elimService: ElimserviceService) { 
+    this.people = this.elimService.getPeople();
+  }
+  public test()
+  {
+    console.log("test");
+  }
 }
--- DeMol/src/app/services/elimservice.service.ts
+++ DeMol/src/app/services/elimservice.service.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ElimserviceService {
-
+  people: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   constructor() { }
+
+  getPeople() {
+    return this.people;
+  }
+  addPerson(person: string) {
+    this.people.push(person);
+  }
+  clearAllPeople() {
+    this.people = [];
+  }
 }
