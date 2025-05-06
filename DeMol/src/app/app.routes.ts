40dc816a957f5c671ce0da55e01241a9bd61385b
Arne <arnev.c.23@gmail.com>
2025-05-06 13:52:23 +0200

    setup routing

--- DeMol/src/app/app.routes.ts
+++ DeMol/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ElimpageComponent } from './elimpage/elimpage.component';
+import { ResultspageComponent } from './resultspage/resultspage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'elim', component: ElimpageComponent },
+    { path: 'result', component: ResultspageComponent },
+];
